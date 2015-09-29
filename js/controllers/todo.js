8c7bae901d114565ba45f452ad1d829b37f3904f
Haroon Khan <aitchkhan@gmai.com>
2015-09-29 12:14:27 +0600

    checking

--- js/controllers/todo.js
+++ js/controllers/todo.js
@@ -18,7 +18,43 @@ angular.module('todo')
 
         // Monitor the current route for changes and adjust the filter accordingly
         $scope.$on('$routeChangeSuccess', function () {
-            var status = $scope.status = $routeParams.status
-        })
+            var status = $scope.status = $routeParams.status || '';
+
+            $scope.statusFilter = (status === 'active') ? { completed: false }
+                : (status === 'completed') ? { completed: true }
+                : {} ;
+        });
+
+        //add new to-do to localStoreage
+        $scope.addTodo = function () {
+            var newToDo = {
+                title: $scope.newTodo.trim(),
+                completed: false
+            };
+
+            if(!newToDo.title) {
+                return ;
+            }
+
+            $scope.saving = true;
+
+            store.insert(newToDo)
+                .then(function success(){
+                    $scope.newTodo = '';
+                })
+                .finally(function () {
+                    $scope.saving = false;
+                });
+
+        };
+
+        $scope.editTodo = function(todo) {
+            $scope.editedTodo = todo;
+
+            //Clone the original todo to restore it on demand
+
+        }
+
+
         
-    })
+    });
